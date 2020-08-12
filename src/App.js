522bf66e46730e7b6ab24c7c9faf504f5de71ab1
mbrad26 <mbrad26@gmail.com>
2020-08-12 15:59:06 +0100

    add data from API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
-import React, { useState } from 'react';
-import ReactMapGl from 'react-map-gl';
+import React, { useState, useEffect } from 'react';
+import ReactMapGl, { Marker } from 'react-map-gl';
 import './App.css';
+
+import axios from 'axios';
+
+const url = 'https://covid19.mathdro.id/api'
 
 const Map = () => {
   const [viewPort, setViewPort] = useState({
@@ -8,7 +12,7 @@ const Map = () => {
     height: '60vh',
     latitude: 48.856613,
     longitude: 2.352222,
-    zoom: 1.45,
+    zoom: 1.85,
   });
 
   return (
@@ -17,15 +21,58 @@ const Map = () => {
       mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
       mapStyle="mapbox://styles/mbrad26/ckdr7xcji0pq619oo5yj3ovuo"
       onViewportChange={newViewport => setViewPort(newViewport)}
-    />
+    >
+      <Marker
+        latitude={48}
+        longitude={2}
+      >
+        <button>Marker</button>
+      </Marker>
+    </ReactMapGl>
   );
 };
 
 const App = () => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false)
+
+  const fetchData = async () => {
+    setIsLoading(true);
+
+    try {
+      const result = await axios.get(url);
+      // console.log(result.data.confirmed.value);
+      setData(result.data);
+      setIsLoading(false)
+    } catch (e) {
+      console.log(e);
+      setIsError(true);
+    }
+  };
+
+  console.log("DATA: ", data);
+
+  useEffect(() => {
+    fetchData();
+  }, [url]);
+
   return (
     <div className="App">
       <h2>Covid-19 Tracker</h2>
       <Map />
+      {isLoading
+        ? <h3>Loading ...</h3>
+        : data.confirmed ? (
+          <>
+            <p>Confirmed:<span> {data.confirmed.value.toLocaleString()}</span></p>
+            <p>Deaths:<span> {data.deaths.value.toLocaleString()}</span></p>
+            <p>Recovered:<span> {data.recovered.value.toLocaleString()}</span></p>
+          </>
+        )
+        : null
+      }
+      {isError && <h3>Something went wrong ...</h3>}
     </div>
   );
 };
