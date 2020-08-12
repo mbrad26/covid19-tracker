5efeaeb08484cceb16d2c7fecf3f3a4cf0519726
mbrad26 <mbrad26@gmail.com>
2020-08-12 10:44:11 +0100

    implemented mapbox viewPort

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ReactMapGl from 'react-map-gl';
 import './App.css';
 
-function App() {
+const Map = () => {
+  const [viewPort, setViewPort] = useState({
+    width: '100vw',
+    height: '100vh',
+    latitude: 50.909698,
+    longitude: -1.404351,
+    zoom: 2,
+  });
+
+  return (
+    <ReactMapGl
+      {...viewPort}
+      mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+      onViewportChange={newViewport => setViewPort(newViewport)}
+    />
+  );
+};
+
+const App = () => {
   return (
     <div className="App">
       Covid-19 Tracker
+      <Map />
     </div>
   );
-}
+};
 
 export default App;
