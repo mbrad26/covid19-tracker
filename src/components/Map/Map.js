5ba0e500a7d70246917dacbb4a741db12dffed4b
mbrad26 <mbrad26@gmail.com>
2020-08-21 18:20:36 +0100

    change cases scale and add opacity

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -8,9 +8,9 @@ const Map = () => {
   const initialState = {
     width: window.innerWidth * 0.80,
     height: window.innerHeight * 0.6,
-    latitude: 45,
+    latitude: 35,
     longitude: 5,
-    zoom: 0.65,
+    zoom: 1.5,
   }
   const [viewPort, setViewPort] = useState(initialState);
   const data = useSelector(state => state.countriesDataState.data);
@@ -18,40 +18,36 @@ const Map = () => {
   console.log('DATA: ', data);
 
   let height, width;
-  // let background = 'background-color';
 
   const stats = cases =>
-    cases < 100
+    cases < 1000
       ? 1
-      : cases < 1000
-      ? 2
       : cases < 5000
-      ? 3
+      ? 2
       : cases < 10000
-      ? 4
+      ? 3
       : cases < 50000
-      ? 5
+      ? 4
       : cases < 100000
-      ? 6
+      ? 5
       : cases < 500000
-      ? 7
+      ? 6
       : cases < 1000000
-      ? 8
+      ? 7
       : cases < 5000000
-      ? 9
-      : 10
+      ? 8
+      : 9
 
 const style = {
-  1: {height: 10 + 'px', width: 10 + 'px', 'background-color': '#fecefe'},
-  2: {height: 10 + 'px', width: 10 + 'px', 'background-color': '#fecefe'},
-  3: {height: 12 + 'px', width: 12 + 'px', 'background-color': '#fecefe'},
-  4: {height: 14 + 'px', width: 14 + 'px', 'background-color': '#fecefe'},
-  5: {height: 16 + 'px', width: 16 + 'px', 'background-color': '#fecefe'},
-  6: {height: 18 + 'px', width: 18 + 'px', 'background-color': '#fecefe'},
-  7: {height: 20 + 'px', width: 20 + 'px', 'background-color': '#fecefe'},
-  8: {height: 22 + 'px', width: 22 + 'px', 'background-color': '#fecefe'},
-  9: {height: 35 + 'px', width: 35 + 'px', 'background-color': '#fecefe'},
-  10: {height: 45 + 'px', width: 45 + 'px', 'background-color': '#fecefe'},
+  1: {height: 14 + 'px', width: 14 + 'px', 'background-color': '#ffffcc'},
+  2: {height: 16 + 'px', width: 16 + 'px', 'background-color': '#ffeda0'},
+  3: {height: 18 + 'px', width: 18 + 'px', 'background-color': '#fed976'},
+  4: {height: 20 + 'px', width: 20 + 'px', 'background-color': '#feb24c'},
+  5: {height: 22 + 'px', width: 22 + 'px', 'background-color': '#fd8d3c'},
+  6: {height: 26 + 'px', width: 26 + 'px', 'background-color': '#fc4e2a'},
+  7: {height: 30 + 'px', width: 30 + 'px', 'background-color': '#e31a1c'},
+  8: {height: 36 + 'px', width: 36 + 'px', 'background-color': '#bd0026'},
+  9: {height: 50 + 'px', width: 50 + 'px', 'background-color': '#800026'},
 }
 
   return (
