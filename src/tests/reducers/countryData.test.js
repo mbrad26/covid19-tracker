f88b4144eb7cffdf9f48304ffe3423c517adfca5
mbrad26 <mbrad26@gmail.com>
2020-08-31 13:42:11 +0100

    add tests for countryDataReducer

---
+++ src/tests/reducers/countryData.test.js
@@ -0,0 +1,25 @@
+import countryDataReducer from '../../reducers/countryData';
+
+describe('countryDataReducer', () => {
+  it('returns state with active object', () => {
+    const data = {};
+    const action = { type: 'SET_ACTIVE', payload: data };
+    const state = { active: null };
+    const expectedState = { active: data };
+
+    const newState = countryDataReducer(state, action);
+
+    expect(newState).toStrictEqual(expectedState);
+  });
+
+  it('returns state with inactive object', () => {
+    const data = {};
+    const action = { type: 'SET_INACTIVE' };
+    const state = { active: data };
+    const expectedState = { active: null };
+
+    const newState = countryDataReducer(state, action);
+
+    expect(newState).toStrictEqual(expectedState);
+  });
+});
