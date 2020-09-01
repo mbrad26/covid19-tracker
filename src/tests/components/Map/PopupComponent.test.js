58b3d7e5a400bae95a25aeb329772eb6740fb3c1
mbrad26 <mbrad26@gmail.com>
2020-09-01 20:35:50 +0100

    add snapshot test PopupComponent

--- src/tests/components/Map/PopupComponent.test.js
+++ src/tests/components/Map/PopupComponent.test.js
@@ -1,4 +1,26 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { render, screen } from '@testing-library/react';
 
+import store from '../../../store/';
+import { country } from '../../fixtures';
 import PopupComponent from '../../../components/Map/PopupComponent';
+
+describe('PopupComponent', () => {
+  const popupProps = {
+    activeCountry: null,
+    onSetInactive: jest.fn(),
+  }
+
+  it('renders snapshot', () => {
+    const { container } = render(
+      <Provider store={store}>
+        <PopupComponent {...popupProps} />
+      </Provider>
+    );
+
+    screen.debug();
+
+    expect(container.firstChild).toMatchSnapshot();
+  });
+});
