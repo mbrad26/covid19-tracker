b5a0010603a7789383f37659d2c07d54a297446a
mbrad26 <mbrad26@gmail.com>
2020-09-01 18:17:43 +0100

    add snapshot test DeathsCard

--- src/tests/components/DataCards/DeathsCard.test.js
+++ src/tests/components/DataCards/DeathsCard.test.js
@@ -2,21 +2,21 @@ import React from 'react';
 // import { Provider } from 'react-redux';
 import { render } from '@testing-library/react';
 
-import { globalData } from '../../fixtures';
+import { globalData, historicalData } from '../../fixtures';
 import DeathsCard from '../../../components/DataCards/DeathsCard';
 
 describe('DeathsCard', () => {
   it('renderes snapshot', () => {
-    // const { container } = render(
-    //   <DeathsCard
-    //     cases={globalData.cases}
-    //     country={{}}
-    //     lastUpdated={''}
-    //     casesIncrease={''}
-    //     historicalData={{}}
-    //   />
-    // );
-    //
-    // expect(container.firstChild).toMatchSnapshot();
+    const { container } = render(
+      <DeathsCard
+        deaths={globalData.deaths}
+        country={globalData.country ? globalData.country : 'Worldwide'}
+        lastUpdated={new Date(globalData.updated).toDateString()}
+        deathsIncrease={globalData.todayDeaths}
+        historicalData={historicalData}
+      />
+    );
+
+    expect(container.firstChild).toMatchSnapshot();
   });
 });
--- src/tests/components/DataCards/RecoveredCard.test.js
+++ src/tests/components/DataCards/RecoveredCard.test.js
@@ -2,21 +2,21 @@ import React from 'react';
 // import { Provider } from 'react-redux';
 import { render } from '@testing-library/react';
 
-import { globalData } from '../../fixtures';
+import { globalData, historicalData } from '../../fixtures';
 import RecoveredCard from '../../../components/DataCards/RecoveredCard';
 
 describe('RecoveredCard', () => {
   it('renderes snapshot', () => {
-  //   const { container } = render(
-  //     <RecoveredCard
-  //       cases={globalData.cases}
-  //       country={{}}
-  //       lastUpdated={''}
-  //       casesIncrease={''}
-  //       historicalData={{}}
-  //     />
-  //   );
-  //
-  //   expect(container.firstChild).toMatchSnapshot();
+    // const { container } = render(
+    //   <RecoveredCard
+    //     recovered={globalData.recovered}
+    //     country={globalData.country ? globalData.country : 'Worldwide'}
+    //     lastUpdated={new Date(globalData.updated).toDateString()}
+    //     recoveredIncrease={globalData.todayRecovered}
+    //     historicalData={historicalData}
+    //   />
+    // );
+    //
+    // expect(container.firstChild).toMatchSnapshot();
   });
 });
