4b71b602ae2decb8c5485bf439e1e00008a8408e
mbrad26 <mbrad26@gmail.com>
2020-09-05 07:43:21 +0100

    add date interval to news query

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,14 @@
+const year = new Date().getFullYear();
+const month = new Date().getMonth();
+const day = new Date().getDay();
+
+const date = `${year}-${month}-${day}`;
+
 export const BASE_URL = 'https://corona.lmao.ninja/v2/';
 export const GLOBAL_URL = `${BASE_URL}all`;
 export const COUNTRIES_URL = `${BASE_URL}countries`;
 export const HISTORICAL_DATA_URL = `${BASE_URL}historical/`;
 
-export const NEWS_URL = `https://newsapi.org/v2/everything?q=covid-19&sources=,associated-press,bbc-news,cnn&apiKey=${process.env.REACT_APP_NEWS_TOKEN}`;
+export const NEWS_URL = `https://newsapi.org/v2/everything?q=covid-19&from=${date}&to=${date}&sources=,associated-press,bbc-news,cnn&apiKey=${process.env.REACT_APP_NEWS_TOKEN}`;
 
 export const NHS_URL = 'https://api.nhs.uk/conditions/coronavirus-covid-19?url=Covid-19%20Tracker&modules=false';
