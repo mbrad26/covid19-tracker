99b2b6445d33ac4c1d304a7ba79504cf058cf95f
mbrad26 <mbrad26@gmail.com>
2020-09-11 17:04:07 +0100

    fix news api URL

--- src/api/api.js
+++ src/api/api.js
@@ -4,11 +4,13 @@ const day = new Date().getDay();
 
 const date = `${year}-${month}-${day}`;
 
+// &from=${date}&to=${date}
+
 export const BASE_URL = 'https://corona.lmao.ninja/v2/';
 export const GLOBAL_URL = `${BASE_URL}all`;
 export const COUNTRIES_URL = `${BASE_URL}countries`;
 export const HISTORICAL_DATA_URL = `${BASE_URL}historical/`;
 
-export const NEWS_URL = `https://newsapi.org/v2/everything?q=covid-19&from=${date}&to=${date}&sources=,associated-press,bbc-news,cnn&apiKey=${process.env.REACT_APP_NEWS_TOKEN}`;
+export const NEWS_URL = `https://newsapi.org/v2/everything?q=covid-19&sources=,associated-press,bbc-news,cnn&apiKey=${process.env.REACT_APP_NEWS_TOKEN}`;
 
 export const NHS_URL = 'https://api.nhs.uk/conditions/coronavirus-covid-19?url=Covid-19%20Tracker&modules=false';
